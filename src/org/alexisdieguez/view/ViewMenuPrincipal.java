4ef346d3d8eeae8e5154056a7cdbc0cb0953eeaf
IntAlexiS67 <adieguez-2023463@kinal.edu.gt>
2026-04-11 13:24:02 -0600

    Update ViewMenuPrincipal.java

--- src/org/alexisdieguez/view/ViewMenuPrincipal.java
+++ src/org/alexisdieguez/view/ViewMenuPrincipal.java
@@ -4,22 +4,39 @@ import java.util.Scanner;
  *
  * @author Alexis Diéguez :D
  */
-public class ViewMenuPrincipal {
-    //global
-    int opcionMenu = 0;
-    Scanner leer = new Scanner(System.in);        
-           public void mostrarMenu(){
-                   //LOCAL
-                   while (opcionMenu == 0){
-                       System.out.println("Bienvenido, Seleccione una opción: ");
-                       System.out.println("1. Operaciones matemáticas");
-                       System.out.println("2. Operaciones de texto");
-                       System.out.println("3. Operaciones de lógica");
-                       System.out.println("4. Salir del sistema");
-                       opcionMenu= Integer.parseInt(leer.nextLine());
-           }
-    }
-    public void mostrarMenuMatematica(){
-        System.out.println("Menú de operaciones matemáticas");
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("\n===== MENU PRINCIPAL =====");
+            System.out.println("1. Operaciones Matematicas");
+            System.out.println("2. Texto");
+            System.out.println("3. Logica ");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opcion: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    ControllerMatematicas.menu(sc);
+                    break;
+                case 2:
+                    ControllerTexto.menu(sc);
+                    break;
+                case 3:
+                    ControllerLogica.menu(sc);
+                    break;
+                case 4:
+                    System.out.println("Saliendo del sistema...");
+                    break;
+                default:
+                    System.out.println("Opcion invalida");
+            }
+
+        } while (opcion != 4);
     }
 }
